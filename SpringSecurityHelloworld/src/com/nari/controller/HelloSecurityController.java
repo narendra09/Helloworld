03ae88c48750a7530df53b965a521db6e51960e3
narendrab <narendrab@NarendraB.planetonline.com>
2018-06-18 19:05:25 +0530

    this is for security

---
+++ SpringSecurityHelloworld/src/com/nari/controller/HelloSecurityController.java
@@ -0,0 +1,24 @@
+package com.nari.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author Dinesh Rajput
+ *
+ */
+@Controller
+@RequestMapping("/index")
+public class HelloSecurityController {
+ 
+ @RequestMapping(method = RequestMethod.GET)
+ public String executeSecurity(ModelMap model) {
+ 
+  model.addAttribute("message", "Spring Security Hello World");
+  model.addAttribute("author", "By DineshOnJava.com");
+  return "welcome";
+ 
+ }
+}
